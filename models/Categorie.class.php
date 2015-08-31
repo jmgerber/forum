c399f0e520b8b3269e4112799bb68fcd34f523f8
jmgerber <jmgerber57@gmail.com>
2015-08-31 16:09:53 +0200

    Ajout

---
+++ models/Categorie.class.php
@@ -0,0 +1,19 @@
+<?php
+class Categorie
+{
+	// Propriétés de la class class Message
+	private $id;
+	private $category;
+
+	// Méthodes de la class Message
+	// GETTER
+	public function getId()
+	{
+		return $this->id;
+	}
+	public function getCategory()
+	{
+		return $this->category;
+	}
+}
+?>
