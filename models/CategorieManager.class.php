0964ac3886a02105b5dc501b02ffbcce443b7ab5
jmgerber <jmgerber57@gmail.com>
2015-09-01 11:57:01 +0200

    Ajout

--- models/CategorieManager.class.php
+++ models/CategorieManager.class.php
@@ -1,25 +1,19 @@
 <?php
 require('models/Categorie.class.php');
-
 class CategorieManager
 {
 	private $link;
-
 	public function __construct($link)
 	{
 		$this->link = $link;
 	}
-	public function select()
+	public function select($id)// On demande a notre manager de récupérer la category numero $id
 	{
-		$request = "SELECT * FROM categorie WHERE id = '".$this->id."'";
+		$request = "SELECT * FROM categorie WHERE id = '".intval($id)."'";// on n'oublie pas le intval, pour la sécurité :)
 		$res = mysqli_query($this->link, $request);
-		$resultat = array();
-		while ($categorie = mysqli_fetch_object($res, 'Categorie', array($this->link)))
-		{
-			$resultat[] = $categorie;
-		}
-		return $resultat;
-
+		// Pas besoin de while ici, vu qu'on ne récup qu'un seul et unique résultat
+		$categorie = mysqli_fetch_object($res, 'Categorie', array($this->link));
+		return $categorie;
 	}
 	public function selectAll()
 	{
