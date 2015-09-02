3f1189e2c6b6cba62497a76ed0dd3605a152ea87
laurent-git <laurent.litt@yahoo.fr>
2015-09-02 11:57:38 +0200

    maj

--- apps/display-topics.php
+++ apps/display-topics.php
@@ -1,3 +1,12 @@
 <?php
+
+$manager = new CategorieManager($link);
+$categorie = $manager->selectByName($_GET['category']);
+var_dump($categorie);
+
 require ('./views/display-topics.phtml');
+	while ($topics = )
+	{
+	require ('./display-topics.phtml')
+	}
 ?>
