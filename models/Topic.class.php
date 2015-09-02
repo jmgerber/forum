7ba21ed6a84d3714baca1074c0c4ba9dff7c2f29
jmgerber <jmgerber57@gmail.com>
2015-09-02 14:31:55 +0200

    MaJ

--- models/Topic.class.php
+++ models/Topic.class.php
@@ -34,8 +34,8 @@ class Topic
 	public function getAuteur()
 	{
 		$manager = new UserManager($this->link);
-		$auteur = $manager->selectById($this->id_auteur);
-		return $auteur;
+		$user = $manager->selectById($this->id_auteur);
+		return $user;
 	}
 	public function getIdCategory()
 	{
@@ -74,7 +74,7 @@ class Topic
 		$res = mysqli_query($this->link, $request);
 		//Récupère l'id de la dernière requête SQL à savoir UPDATE !
 		if ($res)
-			return $this->selectById(mysqli_insert_id($this->link));
+			return $this->select(mysqli_insert_id($this->link));
 		else
 			throw new Exception("Erreur interne du serveur");
 	}
@@ -108,15 +108,6 @@ class Topic
 		}
 		return $resultat;
 	}
-	
-	public function selectById($id)
-	{
-		$request = "SELECT * from messages WHERE id = '".$id."'";
-		$res = mysqli_query($this->link, $request);
-		$message = mysqli_fetch_object($res, "Message", array($this->link));
-		return $message;
-	}
-
 
 	public function selectBySignal()
 	{
