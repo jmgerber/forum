62a073c1e6845322a8da5f32df304c41a6725978
jmgerber <jmgerber57@gmail.com>
2015-09-02 09:23:20 +0200

    MaJ

--- apps/traitement-register.php
+++ apps/traitement-register.php
@@ -1,21 +1,32 @@
 <?php
 require('models/UserManager.class.php');
 $manager = new UserManager($link);
-if(isset($_POST['create'], $_POST['login'], $_POST['email'], $_POST['password'], $_POST['password2'], $_POST['avatar'])
-	&& !empty($_POST['login']));
-{
-	try
-	{
-		if(isset($_POST['login'], $_POST['email'], $_POST['password'], $_POST['avatar'])){
-			$user = $manager->create($_POST['login'], $_POST['email'], $_POST['password'], $_POST['avatar']);
-			header('Location: login');
-			exit;
-		}
 
+try
+{
+	if(isset($_POST['login'], $_POST['email'], $_POST['password'], $_POST['password2'], $_POST['avatar'])){
+		if(empty($_POST['login']) || empty($_POST['email']) || empty($_POST['password']) || empty($_POST['password2']) || empty($_POST['avatar'])){
+			$error = "Il manque des informations";
+		}
+		else
+		{
+			if($_POST['password'] == $_POST['password2']){
+				$user = $manager->create($_POST['login'], $_POST['email'], $_POST['password'], $_POST['avatar']);
+				header('Location: login');
+				exit;
+			}
+			else{
+				$error = "Les deux mots de passe ne correspondent pas";
+			}
+		}
 	}
-	catch (Exception $exception)
-	{
-		$error = $exception->getMessage();
-	}
+
 }
+catch (Exception $exception)
+{
+	$error = $exception->getMessage();
+}
+
+// $login = mysqli_real_escape_string($link, $_POST['login']);
 ?>
+
