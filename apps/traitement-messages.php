bc1712541d1359494b3d585144b30940a7403282
djay76 <djay76@gmail.com>
2015-09-02 13:31:00 +0200

    maj

--- apps/traitement-messages.php
+++ apps/traitement-messages.php
@@ -1,11 +1,23 @@
 <?php
 var_dump($_POST);
 var_dump($_GET);
+
 //Insertion d'un nouveau message
 if (isset($_POST['insert'], $_POST['contenu']))
 {
-	$manager = new Topic($link);
-	$message = $manager->create($_POST['contenu']);
+	$categoryName = $_GET['category'];
+	$topicName = $_GET['topic'];
+
+	//Récupération de la catégorie
+	$manager = new CategorieManager($link);
+	$categorie = $manager->selectByName($categoryName);
+	// var_dump($categorie->getId());
+
+	//Récupération du topic
+	$topic = $categorie->selectByName($topicName);
+	// var_dump($topic->getId());
+
+	$message = $topic->create($_POST['contenu']);
 }
 
 //Mise à jour d'un message
