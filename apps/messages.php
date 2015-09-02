833a4c27ceba55b458c4b8190242e95a4fbc8884
djay76 <djay76@gmail.com>
2015-09-02 09:37:45 +0200

    MAJ

--- apps/messages.php
+++ apps/messages.php
@@ -2,6 +2,7 @@
 // require ('/models/CategorieManager.class.php');
 // $categorie = new CategorieManager($link);
 
-
+$category = urldecode($_GET['category']);
+$topic = urldecode($_GET['topic']);
 require ('views/messages.phtml');
 ?>
