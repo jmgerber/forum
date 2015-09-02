fb061c0af9a28b99bcf420e84630f865e98b6460
djay76 <djay76@gmail.com>
2015-09-02 11:32:20 +0200

    messages

--- apps/messages.php
+++ apps/messages.php
@@ -1,18 +1,15 @@
 <?php
-$category = $_GET['category'];
-$topic = $_GET['topic'];
+$categoryName = $_GET['category'];
+$topicName = $_GET['topic'];
 
 //Récupération de la catégorie
 $manager = new CategorieManager($link);
-$categorie = $manager->selectByName($category);
-var_dump($categorie->getId());
+$categorie = $manager->selectByName($categoryName);
+// var_dump($categorie->getId());
 
 //Récupération du topic
-$topic = $categorie->selectByName($topic);
-var_dump($topic->getId());
+$topic = $categorie->selectByName($topicName);
+// var_dump($topic->getId());
 
-//Récupération des messages
-$messages = $topic->selectAll();
-var_dump($messages);
 require ('views/messages.phtml');
 ?>
