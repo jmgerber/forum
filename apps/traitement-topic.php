6b6086ef6c8b068b4a118619750177321293f7ca
laurent-git <laurent.litt@yahoo.fr>
2015-09-03 15:06:25 +0200

    maj

--- apps/traitement-topic.php
+++ apps/traitement-topic.php
@@ -1,3 +1,19 @@
 <?php
 
+if (isset($_POST['titreTopic'], $_POST['send'], $_SESSION['id'], $_GET['id']))
+{
+	$manager = new CategorieManager($link);
+	$categorie = $manager->select($_GET['id']);
+	try
+	{
+		$categorie->create($_POST['titreTopic']);
+	}
+	catch(Exception  $exception)
+	{
+		$error = $exception->getMessage();
+	}
+
+}
+header ('Location: ./home');
+exit;
 ?>
