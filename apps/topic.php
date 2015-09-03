034621a664d0c31eeb97976bda0d37f327a0f43e
laurent-git <laurent.litt@yahoo.fr>
2015-09-03 09:36:34 +0200

    maj

--- apps/topic.php
+++ apps/topic.php
@@ -1,3 +1,9 @@
 <?php
+$categoryName = $_GET['category'];
+
+//Récupération de la catégorie
+$manager = new CategorieManager($link);
+$categorie = $manager->selectByName($categoryName);
+
 require ('./views/topic.phtml');
 ?>
