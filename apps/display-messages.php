aa804e5c80f00d36d1b718a5332af05754bcafcc
djay76 <djay76@gmail.com>
2015-09-03 11:37:18 +0200

    messages

--- apps/display-messages.php
+++ apps/display-messages.php
@@ -5,15 +5,16 @@ $messages = $topic->selectAll();
 $i=0;
 while (isset($messages[$i]))
 {
-	$contenu = $messages[$i]->getContenu();
+	$message = $messages[$i];
+	// $contenu = $messages[$i]->getContenu();
 	$date = $messages[$i]->getDate();
 	$id = $messages[$i]->getId();
 	$id_user = $messages[$i]->getId_auteur();
 	//Récupération des infos user
 	$usermanager = new UserManager($link);
 	$user = $usermanager->selectById($id_user);
-	$login = $user->getLogin();
-	$avatar = $user->getAvatar();
+	// $login = $user->getLogin();
+	// $avatar = $user->getAvatar();
 	require ('./views/display-messages.phtml');
 	$i++;
 }
