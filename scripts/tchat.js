741b151f4566a118b1a8261e0efe2a5c24363180
djay76 <djay76@gmail.com>
2015-09-04 16:47:03 +0200

    tchat

--- scripts/tchat.js
+++ scripts/tchat.js
@@ -3,12 +3,26 @@ $('#tchatForm').submit(function(e){
     var url = $(this).parents(".full-tchat").find("form").attr("action");
     var message = $(this).parents(".full-tchat").find("#message").val();
 
-    if(message != ""){
-        $(this).parents(".full-tchat").find(".tchat").prepend("<p>" + message + "</p>");
+    if(message != "")
+    {
+        //$(this).parents(".full-tchat").find(".tchat").prepend("<p>" + message + "</p>");
         $.post(url, {message:message, submit:true}, function(data){
+        	refreshMessages(url);
         });
     }
+    return false;
 });
+
+setInterval(function()
+{
+	refreshMessages("http://localhost/forum/tchat");
+}, 1000);
+function refreshMessages(url)
+{
+	$.get(url, {refresh:true}, function(data){
+    	$('.tchat').html(data);
+    });
+}
 
 // function refresh(){
 
