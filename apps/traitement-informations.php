690c473f37f3839ff78816047ff186c86f2dffbf
jmgerber <jmgerber57@gmail.com>
2015-09-04 13:48:01 +0200

    MaJ

--- apps/traitement-informations.php
+++ apps/traitement-informations.php
@@ -1,7 +1,7 @@
 <?php
 $manager = new UserManager($link);
 $user = $manager->selectById($_SESSION['id']);
-if(isset($_POST['update'])){
+if(isset($_POST['update'])){ // Si on envoi le formulaire
 	if(empty($_POST['login']) || empty($_POST['email']) || empty($_POST['avatar']))
 	{
 		$error = "Il manque des informations";
@@ -12,21 +12,21 @@ if(isset($_POST['update'])){
 		$email = mysqli_real_escape_string($link, $_POST['email']);
 		$avatar = mysqli_real_escape_string($link, $_POST['avatar']);
 		
-		if(isset($_POST['oldPassword'],$_POST['newPassword1'], $_POST['newPassword2'])
-		&& !empty($_POST['oldPassword']) && !empty($_POST['newPassword1']) && !empty($_POST['newPassword2']))
+		if(isset($_POST['oldPassword'],$_POST['newPassword1'], $_POST['newPassword2']) // Si les champs de mot de passe sont renseignés
+		&& !empty($_POST['oldPassword']) && !empty($_POST['newPassword1']) && !empty($_POST['newPassword2'])) // et s'ils ne sont pas vides
 		{
 			$oldPassword = $_POST['oldPassword'];
 			$newPassword1 = $_POST['newPassword1'];
 			$newPassword2 = $_POST['newPassword2'];
-			if(password_verify($oldPassword, $user->getPassword()) == TRUE)
+			if(password_verify($oldPassword, $user->getPassword()) == TRUE) // Comparaison avec l'ancien mot de passe
 			{
-				if($newPassword1 !== $newPassword2)
+				if($newPassword1 !== $newPassword2) // Si les 2 nouveaux mots de passe correspondent
 				{
 					$error = "La confirmation du mot de passe n'est pas correcte";
 				}
 				else
 				{
-					$user->setPassword($newPassword1);
+					$user->setPassword($newPassword1); // On met à jour le mot de passe
 				}
 			}
 			else
@@ -34,6 +34,7 @@ if(isset($_POST['update'])){
 				$error = "L'ancien mot de passe ne correspond pas";
 			}
 		}
+		// On met à jour les autres données
 		$user->setLogin($login);
 		$user->setEmail($email);
 		$user->setAvatar($avatar);
