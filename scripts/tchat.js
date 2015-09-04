a172428effa4801c807b7465d7fc29740d47dc51
jmgerber <jmgerber57@gmail.com>
2015-09-04 11:54:38 +0200

    MaJ

--- scripts/tchat.js
+++ scripts/tchat.js
@@ -1,14 +1,14 @@
-// $('#tchatForm').submit(function(e){
-//     e.preventDefault(); // on empêche le bouton d'envoyer le formulaire
-//     var url = $(this).parents(".full-tchat").find("form").attr("action");
-//     var message = $(this).parents(".full-tchat").find("#message").val();
+$('#tchatForm').submit(function(e){
+    e.preventDefault(); // on empêche le bouton d'envoyer le formulaire
+    var url = $(this).parents(".full-tchat").find("form").attr("action");
+    var message = $(this).parents(".full-tchat").find("#message").val();
 
-//     if(message != ""){
-//         $(this).parents(".full-tchat").find(".tchat").prepend("<p>" + message + "</p>");
-//         $.post(url, {message:message, submit:true}, function(data){
-//         });
-//     }
-// });
+    if(message != ""){
+        $(this).parents(".full-tchat").find(".tchat").prepend("<p>" + message + "</p>");
+        $.post(url, {message:message, submit:true}, function(data){
+        });
+    }
+});
 
 // function refresh(){
 
