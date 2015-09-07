964a1575b53718152357cade198e5b0c79d15712
djay76 <djay76@gmail.com>
2015-09-07 16:37:38 +0200

    small updates

--- apps/search.php
+++ apps/search.php
@@ -1,3 +1,10 @@
+<div class="row topic-header">
+	<div class="col-md-5">Sujet des topics</div>
+	<div class="col-md-2">Auteur</div>
+	<div class="col-md-1">Messages</div>
+	<div class="col-md-2">dernier</div>
+	<div class="col-md-2"></div>
+</div>
 <?php
 $i=0;
 while (isset($topics[$i]))
