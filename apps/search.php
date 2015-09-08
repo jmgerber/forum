cfe13b98030298717a13516aa62311336292a91b
djay76 <djay76@gmail.com>
2015-09-08 14:14:23 +0200

    maj

--- apps/search.php
+++ apps/search.php
@@ -1,9 +1,10 @@
 <div class="row topic-header">
 	<div class="col-md-5">Sujet des topics</div>
 	<div class="col-md-1">Auteur</div>
-	<div class="col-md-1">Messages</div>
-	<div class="col-md-3">Date de publication</div>
-	<div class="col-md-2"></div>
+	<div class="col-md-2">Messages</div>
+	<div class="col-md-2">Date de publication</div>
+	<div class="col-md-1"></div>
+	<div class="col-md-1"></div>
 </div>
 <?php
 $i=0;
