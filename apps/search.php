9b28914fead522cd0113a335f8896af5e7300017
djay76 <djay76@gmail.com>
2015-09-09 15:58:41 +0200

    maj

--- apps/search.php
+++ apps/search.php
@@ -1,17 +1,3 @@
-<div class="row topic-header">
-	<div class="col-md-5">Sujet des topics</div>
-	<div class="col-md-1">Auteur</div>
-	<div class="col-md-2">Messages</div>
-	<div class="col-md-2">Date de publication</div>
-	<div class="col-md-1"></div>
-	<div class="col-md-1"></div>
-</div>
-<?php // /!\ DAFUK
-$i=0;
-$length = count($topics);
-while ($i<$length)
-{
-	$topic = $topics[$i];
-	require ('./views/display-topics.phtml');
-	$i++;
-}
+<?php require('views/search.phtml'); ?>
+
+
